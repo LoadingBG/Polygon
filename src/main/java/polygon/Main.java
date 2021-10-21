6725de7507bdd7065c143707a962d52e64e1e736
LoadingBG <troyankru@gmail.com>
2021-10-21 23:24:59 +0300

    Move token to separate file

--- src/main/java/polygon/Main.java
+++ src/main/java/polygon/Main.java
@@ -1,8 +1,20 @@
 package polygon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public final class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String[] args) {
-        final Bot bot = new Bot("ODk5MzEyOTcyNzU1Nzg3Nzk2.YWw8eQ.ye0-RwvRqcXnlh-z_AInC3hN01s");
-        Runtime.getRuntime().addShutdownHook(new Thread(bot::shutdown));
+        try {
+            final String token = new String(Main.class.getResourceAsStream("/token.txt").readAllBytes());
+            final Bot bot = new Bot(token);
+            Runtime.getRuntime().addShutdownHook(new Thread(bot::shutdown));
+        } catch (final IOException e) {
+            LOG.error("Could not read token.", e);
+        }
     }
 }
